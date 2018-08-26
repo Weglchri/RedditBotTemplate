c4194bc8ed726c5a1579c2c5ee4a1f79d46f7c41
Weglchri <christopher.wegl@gmail.com>
2018-08-26 20:19:18 +0200

    modified bot to respond only to new calls

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 import praw
+import time
 
 reddit = praw.Reddit('bot1')
 
 #subreddit = reddit.subreddit("rbtvcirclejerk")
 subreddit = reddit.subreddit('testingground4bots')
+start_time = time.time()
+
 
 #define what the bot should say
 reply_message = "Super important message \n --- \n Posted by bot template \n\n Change this to ... \n\n -- !jep-Bot"
@@ -15,33 +18,32 @@ matcher = "!jep"
 print(reddit.user.me())
 
 
-
 class validator:
     
     def __init__(self):
         self.valid = False
         
     def validateIfBotReplied(self, comment):
-        print("Validator called")
-        
-        
-        for reply in comment.replies:
-            print("Entered Replies")
-            print("Reply: ", reply)
-            #check if already responded
-            
-            self.valid = True
-            
-        return self.valid
+        pass
+#        print("Validator called")
+#        
+#        for reply in comment.replies:
+#            print("Entered Replies")
+#            print("Reply: ", reply)
+#            #check if already responded
+#            self.valid = True
+#            
+#        return self.valid
     
     
 #only for test system
 test_sub_matcher = "testground"
 
 for comment in subreddit.stream.comments():
+    if comment.created_utc > start_time:
         print("Comment: ", comment.body)
         if comment.body == matcher:
-            if validator().validateIfBotReplied(comment):
+#            if validator().validateIfBotReplied(comment):
                 print("Posted Answer!")
                 #comment.reply(reply_message)
                 #comment.refresh()
